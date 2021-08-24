cdd11fa56a6cdf35092648f2db02f2a5c783d2fd
arianspacerock <siropemptyspace@gmail.com>
2021-08-24 18:58:50 +0300

    complete task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
-function eval() {
-    // Do not use eval!!!
-    return;
-}
+function expressionCalculator(express) {
+    let x = 0;
+    express = express.split(' ').join('');
 
-function expressionCalculator(expr) {
-    // write your solution here
+    for (let i = 0; i < express.length; i++) {
+        if (express[i] === '(') x++;
+        if (express[i] === ')') x--;
+    }
+    if (x !== 0) throw new Error("ExpressionError: Brackets must be paired");
+    if (express.includes('/0')) throw new Error("TypeError: Division by zero.");
+    return new Function(`return ${express}`)();
 }
-
 module.exports = {
-    expressionCalculator
-}
+    expressionCalculator,
+};
